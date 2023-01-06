7b2ccabc43b0cbbf55d066a703a233b05ed06df3
Shyamli Garkal <shyamligarkal@gmail.com>
2023-01-06 10:44:21 +0530

    Added sidebar and add student form

--- add_stud.php
+++ add_stud.php
@@ -143,138 +143,138 @@
 
       <?php include 'sidebar.php'; ?>
       <div class="page-wrapper">
-        <div class="content container-fluid">
+      <div class="content container-fluid">
         <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title">Two Column Horizontal Form</h5>
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="card-title">Student</h5>
+              </div>
+              <div class="card-body">
+                <form action="#">
+                  <h5 class="card-title">Personal Information</h5>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                        <label>Gender</label>
+                        <select class="select">
+                          <option>Select</option>
+                          <option value="1">Male</option>
+                          <option value="2">Female</option>
+                          <option value="3">Other</option>
+                        </select>
+                      </div>
+                      <div class="form-group">
+                        <label>Mobile Number</label>
+                        <input type="text" class="form-control" />
+                      </div>
                     </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Personal Information</h5>
-                        <form action="#">
-                            <div class="row">
-                                <div class="col-xl-6">
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">First Name</label>
-                                        <div class="col-lg-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Last Name</label>
-                                        <div class="col-lg-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Gender</label>
-                                        <div class="col-lg-9">
-                                            <div class="form-check form-check-inline">
-                                                <input class="form-check-input" type="radio" name="gender" id="gender_male" value="option1" checked>
-                                                <label class="form-check-label" for="gender_male">
-                                                    Male
-                                                </label>
-                                            </div>
-                                            <div class="form-check form-check-inline">
-                                                <input class="form-check-input" type="radio" name="gender" id="gender_female" value="option2">
-                                                <label class="form-check-label" for="gender_female">
-                                                    Female
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Blood Group</label>
-                                        <div class="col-lg-9">
-                                            <select class="select">
-                                                <option>Select</option>
-                                                <option value="1">A+</option>
-                                                <option value="2">O+</option>
-                                                <option value="3">B+</option>
-                                                <option value="4">AB+</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-6">
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Username</label>
-                                        <div class="col-lg-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Email</label>
-                                        <div class="col-lg-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Password</label>
-                                        <div class="col-lg-9">
-                                            <input type="password" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Repeat Password</label>
-                                        <div class="col-lg-9">
-                                            <input type="password" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <h5 class="card-title">Address</h5>
-                            <div class="row">
-                                <div class="col-xl-6">
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Address Line 1</label>
-                                        <div class="col-lg-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Address Line 2</label>
-                                        <div class="col-lg-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">State</label>
-                                        <div class="col-lg-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-xl-6">
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">City</label>
-                                        <div class="col-lg-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Country</label>
-                                        <div class="col-lg-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                    <div class="form-group row">
-                                        <label class="col-lg-3 col-form-label">Postal Code</label>
-                                        <div class="col-lg-9">
-                                            <input type="text" class="form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="text-end">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                            </div>
-                        </form>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Email</label>
+                        <input type="email" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                        <label>Date of Birth</label>
+                        <input type="date" class="form-control" />
+                      </div>
                     </div>
-                </div>
+                  </div>
+
+                  <h5 class="card-title">Postal Address</h5>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Address Link 1</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                        <label>State</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Address Line 2</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                        <label>Pin Code</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                    </div>
+                  </div>
+
+                  <h5 class="card-title">Center Details</h5>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Affiliate Center Name</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Center Code</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                    </div>
+                  </div>
+
+                  <h5 class="card-title">Other Details</h5>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Photo</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                        <label>Course</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Educational Qualification</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                      <div class="form-group">
+                        <label>Amount</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                    </div>
+                  </div>
+
+                  <h5 class="card-title">Authentication Details</h5>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" class="form-control" />
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="text-end">
+                    <button type="submit" class="btn btn-primary">
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
       </div>
       </div>
     </div>
--- sidebar.php
+++ sidebar.php
@@ -12,10 +12,74 @@
                 <ul>
                   <li><a href="add_stud.php">Add Students</a></li>
                   <li><a href="manage_stud.php">Manage Students</a></li>
-                  
                 </ul>
               </li>
               
+              <li class="submenu">
+                <a href="#"><i data-feather="monitor"></i> <span> Online Exam</span> <span class="menu-arrow"></span></a>
+                <ul>
+                  <li><a href="add_stud.php">Courses</a></li>
+                  <li><a href="manage_stud.php">Instructions</a></li>
+                  <li><a href="manage_stud.php">Online Exams</a></li>
+                  <li><a href="manage_stud.php">Question Banks</a></li>
+                  <li><a href="manage_stud.php">Question Groups</a></li>
+                </ul>
+              </li>
+
+              <li class="submenu">
+                <a href="#"><i data-feather="package"></i> <span> Franchisee</span> <span class="menu-arrow"></span></a>
+                <ul>
+                  <li><a href="add_stud.php">Add Franchisee</a></li>
+                  <li><a href="manage_stud.php">Manage Franchisee</a></li>
+                </ul>
+              </li>
+
+              <li class="submenu">
+                <a href="#"><i data-feather="mail"></i> <span> Message</span> <span class="menu-arrow"></span></a>
+                <ul>
+                  <li><a href="add_stud.php">Add Message</a></li>
+                  <li><a href="manage_stud.php">Manage Message</a></li>
+                </ul>
+              </li>
+
+              <li class="submenu">
+                <a href="#"><i data-feather="users"></i> <span> District Manager</span> <span class="menu-arrow"></span></a>
+                <ul>
+                  <li><a href="add_stud.php">Add District Manager</a></li>
+                  <li><a href="manage_stud.php">Manage District Manager</a></li>
+                </ul>
+              </li>
+
+              <li class="submenu">
+                <a href="#"><i data-feather="alert-circle"></i> <span> Announcement</span> <span class="menu-arrow"></span></a>
+                <ul>
+                  <li><a href="add_stud.php">Latest News</a></li>
+                  <li><a href="manage_stud.php">Notice</a></li>
+                  <li><a href="manage_stud.php">Testimonials</a></li>
+                  <li><a href="manage_stud.php">Question Banks</a></li>
+                  <li><a href="manage_stud.php">Question Groups</a></li>
+                </ul>
+              </li>
+
+              <li class="submenu">
+                <a href="#"><i data-feather="user"></i> <span> Administrator</span> <span class="menu-arrow"></span></a>
+                <ul>
+                  <li><a href="add_stud.php">Student Payment</a></li>
+                  <li><a href="manage_stud.php">Franchisee Payment</a></li>
+                  <li><a href="manage_stud.php">Student Report</a></li>
+                  <li><a href="manage_stud.php">Franchisee Report</a></li>
+                  <li><a href="manage_stud.php">System Admin</a></li>
+                  <li><a href="manage_stud.php">Reset Password</a></li>
+                  <li><a href="manage_stud.php">Backup</a></li>
+                  <li><a href="manage_stud.php">Permission</a></li>
+                  <li><a href="manage_stud.php">Update</a></li>
+                </ul>
+              </li>
+
+              <li >
+                <a href="index.php"><i data-feather="settings"></i> <span>General Settings</span></a>
+              </li>
+
             </ul>
           </div>
         </div>
