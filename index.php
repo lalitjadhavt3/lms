1edfd9cf46c83610a60bccf9003df1fea9da7aee
lalitworkmail <lalitworkmail@gmail.com>
2023-01-06 09:51:01 +0530

    changed heaedr and index

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <!--index-->
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
-    <title>MMITI Admin Dashboard</title>
-
-    <link rel="shortcut icon" href="assets/img/favicon.png" />
-
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css" />
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
-
-    <link rel="stylesheet" href="assets/css/style.css" />
-  </head>
-  <body class="nk-body bg-lighter npc-default has-sidebar no-touch nk-nio-theme">
-    <div class="main-wrapper">
-      <?php include 'header.php'; 
+<?php include 'header.php'; 
       include 'sidebar.php'; ?>
       <div class="page-wrapper">
         <div class="content container-fluid">
@@ -550,7 +531,7 @@
             </div>
           </div>
         </div>
-      </div>
+      </div> <!-- these two opened in header.php -->
     </div>
 
     <script src="assets/js/jquery-3.6.0.min.js"></script>
