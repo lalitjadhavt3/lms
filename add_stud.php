6cfbca4cef6c1965648ee164e86c270e13a9b4da
lalitworkmail <lalitworkmail@gmail.com>
2023-01-06 09:57:42 +0530

    changed blank and add stud layout

--- add_stud.php
+++ add_stud.php
@@ -1,23 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <!--index-->
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />
-    <title>Add Student</title>
-
-    <link rel="shortcut icon" href="assets/img/favicon.png" />
-
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
-
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css" />
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css" />
-
-    <link rel="stylesheet" href="assets/css/style.css" />
-  </head>
-  <body class="nk-body bg-lighter npc-default has-sidebar no-touch nk-nio-theme">
-    <div class="main-wrapper">
-      <?php include 'header.php'; 
+<?php include 'header.php'; 
       include 'sidebar.php'; ?>
       <div class="page-wrapper">
         
